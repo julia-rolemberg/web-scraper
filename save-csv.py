ae7b101dda8a578d2b4941ca087ef08438a7b4f3
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-06-22 14:51:44 -0300

    first commit

---
+++ save-csv.py
@@ -0,0 +1,23 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+
+page = requests.get('https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+last_links = soup.find(class_='AlphaNav')
+last_links.decompose()
+
+f = csv.writer(open('z-artist-names.csv', 'w'))
+f.writerow(['Name', 'Link'])
+
+artist_name_list = soup.find(class_='BodyText')
+artist_name_list_items = artist_name_list.find_all('a')
+
+for artist_name in artist_name_list_items:
+    names = artist_name.contents[0]
+    links = 'https://web.archive.org' + artist_name.get('href')
+
+    f.writerow([names, links])
